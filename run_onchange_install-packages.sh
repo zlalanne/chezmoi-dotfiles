aaa24f6004cc3f88c4c4b3c51584391086fca3cb
Zack Lalanne <zack.lalanne@gmail.com>
2023-04-28 04:03:38 -0500

    Add deps for luasnip

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -r DEBIAN_PKGS="stow fzf exa kitty clang"
+declare -r DEBIAN_PKGS="stow fzf exa kitty clang libluajit-5.1-dev make"
 
 sudo apt update
 sudo apt install -y ${DEBIAN_PKGS}
